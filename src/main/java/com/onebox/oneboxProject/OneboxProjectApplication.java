df8c93533d7cb0629d703967446491af7237226c
Joan Matarredona <53258847+zoni7@users.noreply.github.com>
2025-02-07 16:27:19 +0100

    Hello world

---
+++ src/main/java/com/onebox/oneboxProject/OneboxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onebox.oneboxProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneboxProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onebox/oneboxProject/OneboxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebox.oneboxProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneboxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
